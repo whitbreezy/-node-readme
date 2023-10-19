d2d0dd41b7a5d7f9f97275ecec13da0ade1e611e
whitbreezy <whitneybrady20@gmail.com>
2023-10-18 20:59:04 -0600

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is the title of your project?",
+    "Enter a description for your project", 
+    "Enter installation instructions for your project",
+    "Enter usage information for your project",
+    "Enter contribution guidelines for your project",
+    "Enter test instructions for your project"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
